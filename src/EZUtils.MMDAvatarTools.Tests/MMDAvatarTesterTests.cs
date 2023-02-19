2bd4901d78959f1e79c13f9343b4223a785e9907
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2023-02-19 02:50:24 -0800

    Add test project, tho it doesnt do anything yet

---
+++ src/EZUtils.MMDAvatarTools.Tests/MMDAvatarTesterTests.cs
@@ -0,0 +1,16 @@
+namespace EZUtils.MMDAvatarTools.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.TestTools;
+
+    public class MMDAvatarTesterTests
+    {
+        [Test]
+        [RequiresPlayMode]
+        public void voo(){}
+    }
+}
