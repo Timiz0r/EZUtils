050a1acd1555dd0b3c2c793a707ef31ffcb3dd1b
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2023-02-19 22:11:04 -0800

    Refactor mmdavatartester tests

--- src/EZUtils.MMDAvatarTools.Tests/MMDAvatarTesterTests.cs
+++ src/EZUtils.MMDAvatarTools.Tests/MMDAvatarTesterTests.cs
@@ -11,7 +11,11 @@ namespace EZUtils.MMDAvatarTools.Tests
 
     public class MMDAvatarTesterTests
     {
-
+        //note for waiting:
+        //wanted to wait certain amounts of frames to match what's in the clip, but it appears animations don't work that way
+        //waiting for a period of time seems the most reliable, at least on my machine, but perhaps we'll need to
+        //change it if flaky. probably the best way would be to control time to either the underlying graph or via
+        //Time class. or maybe limiting to 60fps (but then what happens in the unlikely event that we're under?).
         [TearDown]
         public void TearDown() => EZUtils.TestUtils.TestUtils.ClearScene();
 
@@ -19,43 +23,19 @@ namespace EZUtils.MMDAvatarTools.Tests
         [RequiresPlayMode]
         public IEnumerator Start_ChangesShapeKeys_WhenAvatarDescriptorIsDefault()
         {
-            //the robot avatar that comes with the sdk
-            GameObject avatarObject =
-                (GameObject)PrefabUtility.InstantiatePrefab(
-                    AssetDatabase.LoadAssetAtPath<GameObject>(
-                        "Packages/com.vrchat.avatars/Samples/Dynamics/Robot Avatar/Tutorial_Robot_Avatar_Dynamics_Demo_v1.fbx"));
-            VRCAvatarDescriptor avatar = avatarObject.AddComponent<VRCAvatarDescriptor>();
-            MMDAvatarTester mmdAvatarTester = new MMDAvatarTester();
-            AnimationClip animation =
-                AssetDatabase.LoadAssetAtPath<AnimationClip>(
-                    "Packages/com.timiz0r.ezutils.mmdavatartools.tests/mmdsample_vrcrobot.anim");
-            Camera camera = new GameObject("cam").AddComponent<Camera>();
-            camera.transform.SetPositionAndRotation(new Vector3(0, 1.5f, 1), Quaternion.Euler(0, 180, 0));
+            TestSetup testSetup = new TestSetup();
 
-            Light light = new GameObject("light").AddComponent<Light>();
-            light.type = LightType.Directional;
-
-            mmdAvatarTester.Start(avatar, animation);
-            //wanted to wait certain amounts of frames to match what's in the clip, but it appears animations don't work that way
-            //waiting for a period of time seems the most reliable, at least on my machine, but perhaps we'll need to
-            //change it if flaky. probably the best way would be to control time to either the underlying graph or via
-            //Time class. or maybe limiting to 60fps (but then what happens in the unlikely event that we're under?).
+            testSetup.StartMMDTester();
             yield return new WaitForSeconds(0.5f);
 
-            SkinnedMeshRenderer body = avatarObject.transform.Find("Body").GetComponent<SkinnedMeshRenderer>();
-            float blinkWeight = body.GetBlendShapeWeight(body.sharedMesh.GetBlendShapeIndex("blink_both"));
-            Assert.That(blinkWeight, Is.InRange(90f, 110f));
+            Assert.That(testSetup.GetBlendShapeWeight("blink_both"), Is.InRange(90f, 110f));
         }
+
         [UnityTest]
         [RequiresPlayMode]
         public IEnumerator Start_DoesNotChangeShapeKeys_WhenFXLayerHasAStateWithWriteDefaultsOff()
         {
-            //the robot avatar that comes with the sdk
-            GameObject avatarObject =
-                (GameObject)PrefabUtility.InstantiatePrefab(
-                    AssetDatabase.LoadAssetAtPath<GameObject>(
-                        "Packages/com.vrchat.avatars/Samples/Dynamics/Robot Avatar/Tutorial_Robot_Avatar_Dynamics_Demo_v1.fbx"));
-            VRCAvatarDescriptor avatar = avatarObject.AddComponent<VRCAvatarDescriptor>();
+            TestSetup testSetup = new TestSetup();
             VrcDefaultAnimatorControllers animatorControllers = new VrcDefaultAnimatorControllers();
             AnimatorControllerLayer layer = new AnimatorControllerLayer
             {
@@ -70,7 +50,7 @@ namespace EZUtils.MMDAvatarTools.Tests
                 motion = new AnimationClip()
             }, Vector3.zero);
             animatorControllers.FX.AddLayer(layer);
-            avatar.baseAnimationLayers = new[]
+            testSetup.Avatar.baseAnimationLayers = new[]
             {
                 new CustomAnimLayer
                 {
@@ -80,26 +60,46 @@ namespace EZUtils.MMDAvatarTools.Tests
                     animatorController = animatorControllers.FX
                 }
             };
-            MMDAvatarTester mmdAvatarTester = new MMDAvatarTester();
-            AnimationClip animation =
-                AssetDatabase.LoadAssetAtPath<AnimationClip>(
-                    "Packages/com.timiz0r.ezutils.mmdavatartools.tests/mmdsample_vrcrobot.anim");
-            Camera camera = new GameObject("cam").AddComponent<Camera>();
-            camera.transform.SetPositionAndRotation(new Vector3(0, 1.5f, 1), Quaternion.Euler(0, 180, 0));
 
-            Light light = new GameObject("light").AddComponent<Light>();
-            light.type = LightType.Directional;
-
-            mmdAvatarTester.Start(avatar, animation);
-            //wanted to wait certain amounts of frames to match what's in the clip, but it appears animations don't work that way
-            //waiting for a period of time seems the most reliable, at least on my machine, but perhaps we'll need to
-            //change it if flaky. probably the best way would be to control time to either the underlying graph or via
-            //Time class. or maybe limiting to 60fps (but then what happens in the unlikely event that we're under?).
+            testSetup.StartMMDTester();
             yield return new WaitForSeconds(0.5f);
 
-            SkinnedMeshRenderer body = avatarObject.transform.Find("Body").GetComponent<SkinnedMeshRenderer>();
-            float blinkWeight = body.GetBlendShapeWeight(body.sharedMesh.GetBlendShapeIndex("blink_both"));
-            Assert.That(blinkWeight, Is.EqualTo(0f));
+            Assert.That(testSetup.GetBlendShapeWeight("blink_both"), Is.EqualTo(0f));
+        }
+
+        private class TestSetup
+        {
+            private static readonly AnimationClip animation =
+                AssetDatabase.LoadAssetAtPath<AnimationClip>(
+                    "Packages/com.timiz0r.ezutils.mmdavatartools.tests/mmdsample_vrcrobot.anim");
+            private readonly MMDAvatarTester mmdAvatarTester = new MMDAvatarTester();
+
+            public VRCAvatarDescriptor Avatar { get; }
+
+            public TestSetup()
+            {
+                //the robot avatar that comes with the sdk
+                GameObject avatarObject = (GameObject)PrefabUtility.InstantiatePrefab(
+                    AssetDatabase.LoadAssetAtPath<GameObject>(
+                        "Packages/com.vrchat.avatars/Samples/Dynamics/Robot Avatar/Tutorial_Robot_Avatar_Dynamics_Demo_v1.fbx"));
+                Avatar = avatarObject.AddComponent<VRCAvatarDescriptor>();
+
+                //to make things more easily viewable in the game view
+                Camera camera = new GameObject("cam").AddComponent<Camera>();
+                camera.transform.SetPositionAndRotation(new Vector3(0, 1.5f, 1), Quaternion.Euler(0, 180, 0));
+
+                Light light = new GameObject("light").AddComponent<Light>();
+                light.type = LightType.Directional;
+            }
+
+            public void StartMMDTester() => mmdAvatarTester.Start(Avatar, animation);
+
+            public float GetBlendShapeWeight(string blendShapeName)
+            {
+                SkinnedMeshRenderer body = Avatar.transform.Find("Body").GetComponent<SkinnedMeshRenderer>();
+                float result = body.GetBlendShapeWeight(body.sharedMesh.GetBlendShapeIndex(blendShapeName));
+                return result;
+            }
         }
     }
 }
