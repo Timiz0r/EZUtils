b4089bdc93b6a0211ae2c0f944e38943afd6dd58
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2023-02-21 05:07:09 -0800

    Decided to go full-manual approach for now
    It's the best of all the poor solutions, unfortunately.

--- src/EZUtils.MMDAvatarTools/VrcDefaultAnimatorControllers.cs
+++ src/EZUtils.MMDAvatarTools/VrcDefaultAnimatorControllers.cs
@@ -3,11 +3,9 @@ namespace EZUtils.MMDAvatarTools
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using NUnit.Framework;
     using UnityEditor;
     using UnityEditor.Animations;
     using UnityEngine;
-    using static UnityEngine.UI.Image;
     using Object = UnityEngine.Object;
 
     //is not static to allow workflows where these loaded assets are then modified.
@@ -56,16 +54,16 @@ namespace EZUtils.MMDAvatarTools
             return DeepCopy(original);
         }
 
-        //TODO: needs pretty heavy testing.
         //could also try to create a more generic version using SO more. the hard part is know what to copy.
         //for instance, we dont copy motions or avatarmasks, but they would certainly be object references.
         //maybe checking assetdatabase to see if it's a subasset would work.
-        //this still causes innocuous changes, though
+        //not opposed to reflection, but, as long as we do a validation pass when upgrading sdk, this kinda has
+        //better peace-of-mind.
         private static AnimatorController DeepCopy(AnimatorController original)
         {
             Dictionary<Object, Object> objectMap = new Dictionary<Object, Object>();
             HashSet<Object> traversedObjects = new HashSet<Object>();
-            AnimatorController copy = Copy(original);
+            AnimatorController copy = CopyAnimatorController();
 
             AnimatorControllerLayer[] layers = original.layers;
             foreach (AnimatorControllerLayer layer in layers)
@@ -93,56 +91,165 @@ namespace EZUtils.MMDAvatarTools
                         targetIterator.objectReferenceValue = newObject;
                     }
                 }
-                //this actually breaks if done without undo, which is probably a hint as to where to micro changes come from
-                _ = so.ApplyModifiedProperties();
             }
 
             return copy;
 
+            AnimatorController CopyAnimatorController()
+            {
+                AnimatorController newAnimatorController = new AnimatorController()
+                {
+                    hideFlags = original.hideFlags,
+                    name = original.name,
+                    parameters = CopyAll(original.parameters, p => new AnimatorControllerParameter()
+                    {
+                        defaultBool = p.defaultBool,
+                        defaultFloat = p.defaultFloat,
+                        defaultInt = p.defaultInt,
+                        name = p.name,
+                        type = p.type,
+                    }),
+                    layers = CopyAll(original.layers, l => new AnimatorControllerLayer()
+                    {
+                        avatarMask = l.avatarMask,
+                        name = l.name,
+                        blendingMode = l.blendingMode,
+                        defaultWeight = l.defaultWeight,
+                        iKPass = l.iKPass,
+                        stateMachine = CopyStateMachine(l.stateMachine),
+                        syncedLayerAffectsTiming = l.syncedLayerAffectsTiming,
+                        syncedLayerIndex = l.syncedLayerIndex,
+                    })
+                };
+
+                objectMap[original] = newAnimatorController;
+                return newAnimatorController;
+            }
+
             AnimatorStateMachine CopyStateMachine(AnimatorStateMachine originalStateMachine)
             {
-                //get asserts when object instantiating state machines
-                AnimatorStateMachine newStateMachine = new AnimatorStateMachine();
-                EditorUtility.CopySerialized(originalStateMachine, newStateMachine);
+                //cannot object.instantiate AnimatorStateMachine due to an assertion of unknown cause 🤷‍
+                AnimatorStateMachine newStateMachine = new AnimatorStateMachine()
+                {
+                    anyStatePosition = originalStateMachine.anyStatePosition,
+                    name = originalStateMachine.name,
+                    anyStateTransitions = CopyAll(originalStateMachine.anyStateTransitions, t => CopyAnimatorStateTransition(t)),
+                    entryTransitions = CopyAll(originalStateMachine.entryTransitions, t => CopyAnimatorTransition(t)),
+                    states = originalStateMachine.states
+                        .Select(cas => new ChildAnimatorState()
+                        {
+                            position = cas.position,
+                            state = CopyState(cas.state)
+                        })
+                        .ToArray(),
+                    stateMachines = originalStateMachine.stateMachines
+                        .Select(casm => new ChildAnimatorStateMachine()
+                        {
+                            position = casm.position,
+                            stateMachine = CopyStateMachine(casm.stateMachine)
+                        })
+                        .ToArray(),
+                    behaviours = CopyAll(originalStateMachine.behaviours, b => CopyBehaviour(b)),
+                    defaultState = originalStateMachine.defaultState,
+                    entryPosition = originalStateMachine.entryPosition,
+                    exitPosition = originalStateMachine.exitPosition,
+                    hideFlags = originalStateMachine.hideFlags,
+                    parentStateMachinePosition = originalStateMachine.parentStateMachinePosition,
+                };
+
                 objectMap[originalStateMachine] = newStateMachine;
-
-                newStateMachine.states = CopyAll(
-                    originalStateMachine.states, casm => new ChildAnimatorState()
-                    {
-                        position = casm.position,
-                        state = CopyState(casm.state)
-                    });
-                newStateMachine.stateMachines = CopyAll(
-                    originalStateMachine.stateMachines, casm => new ChildAnimatorStateMachine()
-                    {
-                        position = casm.position,
-                        stateMachine = CopyStateMachine(casm.stateMachine)
-                    })
-                    .ToArray();
-                newStateMachine.anyStateTransitions =
-                    originalStateMachine.anyStateTransitions.Select(t => Copy(t)).ToArray();
-                newStateMachine.entryTransitions =
-                    originalStateMachine.entryTransitions.Select(t => Copy(t)).ToArray();
-
                 return newStateMachine;
             }
 
             AnimatorState CopyState(AnimatorState originalState)
             {
-                AnimatorState newState = Copy(originalState);
-                newState.transitions = CopyAll(newState.transitions, t => Copy(t));
-                newState.behaviours = CopyAll(newState.behaviours, b => Copy(b));
+                AnimatorState newState = new AnimatorState()
+                {
+                    behaviours = CopyAll(originalState.behaviours, b => CopyBehaviour(b)),
+                    transitions = CopyAll(originalState.transitions, t => CopyAnimatorStateTransition(t)),
+                    cycleOffset = originalState.cycleOffset,
+                    cycleOffsetParameter = originalState.cycleOffsetParameter,
+                    cycleOffsetParameterActive = originalState.cycleOffsetParameterActive,
+                    hideFlags = originalState.hideFlags,
+                    iKOnFeet = originalState.iKOnFeet,
+                    mirror = originalState.mirror,
+                    mirrorParameter = originalState.mirrorParameter,
+                    mirrorParameterActive = originalState.mirrorParameterActive,
+                    motion = originalState.motion,
+                    name = originalState.name,
+                    speed = originalState.speed,
+                    speedParameter = originalState.speedParameter,
+                    speedParameterActive = originalState.speedParameterActive,
+                    tag = originalState.tag,
+                    timeParameter = originalState.timeParameter,
+                    timeParameterActive = originalState.timeParameterActive,
+                    writeDefaultValues = originalState.writeDefaultValues,
+                };
+
+                objectMap[originalState] = newState;
                 return newState;
             }
 
-            T Copy<T>(T originalObject) where T : Object
+            AnimatorStateTransition CopyAnimatorStateTransition(AnimatorStateTransition originalTransition)
             {
-                T newObject = Object.Instantiate(originalObject);
-                newObject.name = originalObject.name;
-                objectMap[originalObject] = newObject;
-                return newObject;
+                AnimatorStateTransition newTransition = new AnimatorStateTransition()
+                {
+                    canTransitionToSelf = originalTransition.canTransitionToSelf,
+                    duration = originalTransition.duration,
+                    exitTime = originalTransition.exitTime,
+                    hasExitTime = originalTransition.hasExitTime,
+                    hasFixedDuration = originalTransition.hasFixedDuration,
+                    interruptionSource = originalTransition.interruptionSource,
+                    offset = originalTransition.offset,
+                    orderedInterruption = originalTransition.orderedInterruption,
+                };
+                CopyBaseTransitionMembers(originalTransition, newTransition);
+
+                objectMap[originalTransition] = newTransition;
+                return newTransition;
             }
 
+            AnimatorTransition CopyAnimatorTransition(AnimatorTransition originalTransition)
+            {
+                //even though these assignments are duplicated in CopyBaseTransitionMembers,
+                //we have them here as well to make it easier to verify we got them all
+                AnimatorTransition newTransition = new AnimatorTransition()
+                {
+                    destinationState = originalTransition.destinationState,
+                    destinationStateMachine = originalTransition.destinationStateMachine,
+                    name = originalTransition.name,
+                    conditions = originalTransition.conditions,
+                    hideFlags = originalTransition.hideFlags,
+                    isExit = originalTransition.isExit,
+                    mute = originalTransition.mute,
+                    solo = originalTransition.solo,
+                };
+                CopyBaseTransitionMembers(originalTransition, newTransition);
+
+                objectMap[originalTransition] = newTransition;
+                return newTransition;
+            }
+
+            void CopyBaseTransitionMembers(
+                AnimatorTransitionBase originalTransition, AnimatorTransitionBase newTransition)
+            {
+                //dont copy states or state machines, since we do that as part of this transition's state machine
+                newTransition.destinationState = originalTransition.destinationState;
+                newTransition.destinationStateMachine = originalTransition.destinationStateMachine;
+                newTransition.name = originalTransition.name;
+                newTransition.conditions = originalTransition.conditions;
+                newTransition.hideFlags = originalTransition.hideFlags;
+                newTransition.isExit = originalTransition.isExit;
+                newTransition.mute = originalTransition.mute;
+                newTransition.solo = originalTransition.solo;
+            }
+
+            StateMachineBehaviour CopyBehaviour(StateMachineBehaviour originalObject)
+            {
+                StateMachineBehaviour newObject = Object.Instantiate(originalObject);
+                newObject.name = originalObject.name;
+                return newObject;
+            }
             T[] CopyAll<T>(T[] originalObjects, Func<T, T> copier)
                 => originalObjects.Select(o => copier(o)).ToArray();
         }
